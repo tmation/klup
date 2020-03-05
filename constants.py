c882000949cf08df9314178bb2b0826270c8b4c6
maxomator13 <max@tmation.com>
2020-03-05 07:09:41 +0100

    new funcs

---
+++ constants.py
@@ -0,0 +1,13 @@
+COLUMNS_INSTALL_REPORT_ORIGINAL = [
+    'Date',
+    'Daily User Installs',
+    'Daily User Uninstalls',
+    'Active Device Installs'
+]
+
+COLUMS_INSTALL_REPORT_FINAL = [
+    'day',
+    'user_installs',
+    'user_uninstalls',
+    'active_device_installs',
+]
