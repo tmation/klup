a7bb8d9d9a24f4b76b527330d8711360b413fdf6
Max Hofmann <max.hofmann@uber.com>
2020-03-10 15:58:36 +0100

    updated run script

--- func_util.py
+++ func_util.py
@@ -1,10 +1,65 @@
 import sys
+
 sys.path.append('..')
 sys.path.append('/submodules')
+sys.path.append('/sql')
 
 import config
 from appfigures_py import appfigures_py
+from sql import agg_daily_app_store_data
 
-af = appfigures_py.AppFigures(config.appfigures_secrets['client_id'], config.appfigures_secrets['client_secret'])
-af.get_session(config.appfigures_secrets['access_token'], config.appfigures_secrets['access_token_secret'])
+from datetime import datetime, timedelta
+import mysql.connector
+import pandas as pd
+
+af = appfigures_py.AppFigures(
+    config.AF_CLIENT_ID,
+    config.AF_CLIENT_SECRET,
+    config.AF_ACCESS_TOKEN,
+    config.AF_ACCESS_TOKEN_SECRET,
+)
+
+conn = mysql.connector.connect(
+    user=config.DB_KLUP_USER,
+    password=config.DB_KLUP_PASSWORD,
+    host=config.DB_KLUP_HOST,
+    database=config.DB_KLUP_NAME
+)
+
+def convert_keys_to_string(dictionary):
+    """Recursively converts dictionary keys to strings."""
+    if not isinstance(dictionary, dict):
+        return dictionary
+    return dict((str(k), convert_keys_to_string(v))
+        for k, v in dictionary.items())
+
+def get_store_reports_per_day(start_date):
+    end_date = (datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=1)).strftime('%Y-%m-%d')
+    report = af.get_downloads(start_date, end_date)
+
+    store_report_list = []
+    for store in report.keys():
+        store_report = report[store]
+        store_report.update({'day': start_date})
+
+        for k, v in store_report.items():
+            try:
+                if isinstance(v, unicode):
+                    store_report[k] = float(v)
+            except:
+                try:
+                    store_report[k] = str(v)
+                except:
+                    pass
+
+        store_report_list.append(store_report)
+
+    return store_report_list
+
+def replace_data(sql, data, conn):
+    data = convert_keys_to_string(data)
+    cursor = conn.cursor()
+    cursor.execute(sql, data)
+    conn.commit()
+
 
---
+++ run.py
@@ -0,0 +1,35 @@
+import config
+import func_util as fu
+from sql import agg_daily_app_store_data
+
+import datetime
+import mysql.connector
+
+
+conn = mysql.connector.connect(
+    user=config.DB_KLUP_USER,
+    password=config.DB_KLUP_PASSWORD,
+    host=config.DB_KLUP_HOST,
+    database=config.DB_KLUP_NAME
+)
+
+
+
+start = datetime.datetime.strptime("2020-01-01", "%Y-%m-%d")
+end = datetime.datetime.strptime("2020-03-10", "%Y-%m-%d")
+date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+date_list = []
+for date in date_generated:
+    date_list.append(date.strftime("%Y-%m-%d"))
+
+for date in date_list:
+    data_stores = fu.get_store_reports_per_day(date)
+    data = fu.convert_keys_to_string(data_stores[0])
+
+    for data in data_stores:
+        print(data)
+        fu.replace_data(agg_daily_app_store_data.replace, data, conn)
+
+conn.close()
+
