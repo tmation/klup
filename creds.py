6f15d54a78187c97fce2f155b467ed21019feceb
maxomator13 <max@tmation.com>
2020-03-11 18:05:37 +0100

    cleaned up

--- creds.py
+++
@@ -1,9 +0,0 @@
-AF_ACCESS_TOKEN = 'ajdaYJmpymSYuIh1'
-AF_ACCESS_TOKEN_SECRET = 'DUSHX3QYbjyAsLPb'
-AF_CLIENT_ID = 'cf4db30b22c54c1cb73ea090185bac74'
-AF_CLIENT_SECRET = '6463ff3a2f6f4251a14f8bd666ac97b4'
-
-DB_KLUP_HOST = 'klupmariadb.mariadb.database.azure.com'
-DB_KLUP_NAME = 'klup_tmation'
-DB_KLUP_PASSWORD = 'lb3SKHtrlumQ'
-DB_KLUP_USER = 'tmation_staging@klupmariadb'
