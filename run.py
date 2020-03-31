14ac8f0c9a6046b85ed5b3ee5d40e9aab78279b8
maxomator13 <max@tmation.com>
2020-03-31 21:35:00 +0200

    added run file

--- run.py
+++ run.py
@@ -1,33 +1,24 @@
 import config
-import func_util as fu
-from sql import agg_daily_app_store_data
 
-import datetime
 import mysql.connector
-
+import pandas as pd
 
 conn = mysql.connector.connect(
     user=config.DB_KLUP_USER,
     password=config.DB_KLUP_PASSWORD,
     host=config.DB_KLUP_HOST,
-    database=config.DB_KLUP_NAME
+    database=config.DB_KLUP_NAME,
 )
 
-start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
-end = datetime.datetime.strptime("2019-01-02", "%Y-%m-%d")
-date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+with open('sql/analytics_user_base_daily/analytics_user_base_daily.ddl','r') as file:
+    ddl = file.read()
 
-date_list = []
-for date in date_generated:
-    date_list.append(date.strftime("%Y-%m-%d"))
+with open('sql/analytics_user_base_daily/analytics_user_base_daily.sql','r') as file:
+    sql = file.read()
 
-for date in date_list:
-    data_stores = fu.get_store_reports_per_day(date)
-    # data = fu.convert_keys_to_string(data_stores[0])
-
-    for data in data_stores:
-        print(data)
-        fu.replace_data(agg_daily_app_store_data.replace, data, conn)
+cur = conn.cursor()
+cur.execute(ddl)
+df = pd.DataFrame(cur.fetchall())
+df.columns = cur.column_names
 
 conn.close()
-
