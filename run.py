b56710c83156b0ac7cf7084b6944c32e316dae71
maxomator13 <max@tmation.com>
2020-04-01 11:42:51 +0200

    changed run test

--- run.py
+++ run.py
@@ -13,10 +13,12 @@ conn = mysql.connector.connect(
     database=config.DB_KLUP_NAME
 )
 
-cursor = conn.cursor()
-cursor.execute('SELECT MAX(day) FROM klup_tmation.agg_daily_app_store_data')
-print(cursor.fetchall())
+# SELECT TEST
+# cursor = conn.cursor()
+# cursor.execute('SELECT MAX(day) FROM klup_tmation.agg_daily_app_store_data')
+# print(cursor.fetchall())
 
+# API DATA TEST
 # start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
 # end = datetime.datetime.strptime("2019-01-02", "%Y-%m-%d")
 # date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
@@ -33,5 +35,20 @@ print(cursor.fetchall())
 #         print(data)
 #         fu.replace_data(agg_daily_app_store_data.replace, data, conn)
 
+# MODELLING TEST
+with open('sql/analytics_user_base_daily/analytics_user_base_daily.ddl', 'r') as file:
+    ddl = file.read()
+
+with open('sql/analytics_user_base_daily/analytics_user_base_daily.sql', 'r') as file:
+    sql = file.read()
+
+cur = conn.cursor()
+
+cur.execute(ddl)
+cur.execute(sql)
+
+conn.commit()
+
+cur.close()
 conn.close()
 
