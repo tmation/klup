037585766203ecf1a9a5aa0ef45fe82dbf269a79
maxomator13 <max@tmation.com>
2020-04-01 11:38:43 +0200

    changed run to test

--- run.py
+++ run.py
@@ -13,21 +13,25 @@ conn = mysql.connector.connect(
     database=config.DB_KLUP_NAME
 )
 
-start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
-end = datetime.datetime.strptime("2019-01-02", "%Y-%m-%d")
-date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+cursor = conn.cursor()
+cursor.execute('SELECT MAX(day) FROM klup_tmation.agg_daily_app_store_data')
+print(cursor.fetchall())
 
-date_list = []
-for date in date_generated:
-    date_list.append(date.strftime("%Y-%m-%d"))
-
-for date in date_list:
-    data_stores = fu.get_store_reports_per_day(date)
-    # data = fu.convert_keys_to_string(data_stores[0])
-
-    for data in data_stores:
-        print(data)
-        fu.replace_data(agg_daily_app_store_data.replace, data, conn)
+# start = datetime.datetime.strptime("2019-01-01", "%Y-%m-%d")
+# end = datetime.datetime.strptime("2019-01-02", "%Y-%m-%d")
+# date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+#
+# date_list = []
+# for date in date_generated:
+#     date_list.append(date.strftime("%Y-%m-%d"))
+#
+# for date in date_list:
+#     data_stores = fu.get_store_reports_per_day(date)
+#     # data = fu.convert_keys_to_string(data_stores[0])
+#
+#     for data in data_stores:
+#         print(data)
+#         fu.replace_data(agg_daily_app_store_data.replace, data, conn)
 
 conn.close()
 
