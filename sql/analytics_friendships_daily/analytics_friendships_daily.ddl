fcd4f2b4503247d5f7b5d3b73009d428c8b4a497
maxomator13 <max@tmation.com>
2020-04-10 12:08:27 +0200

    added more time dims to friendships table

--- sql/analytics_friendships_daily/analytics_friendships_daily.ddl
+++ sql/analytics_friendships_daily/analytics_friendships_daily.ddl
@@ -3,6 +3,14 @@ CREATE TABLE IF NOT EXISTS `{db_name}`.`{table_name}` (
 	datestr varchar(10),
 	friendships_active integer,
 	friendships_created integer,
+	friendships_created_1d integer,
+	friendships_created_7d integer,
+	friendships_created_14d integer,
+	friendships_created_30d integer,
+	friendships_created_60d integer,
+	friendships_created_90d integer,
+	friendships_created_180d integer,
+	friendships_created_cur_month integer,
 
 	_loaded_at DATETIME,
 
--- sql/analytics_friendships_daily/analytics_friendships_daily.sql
+++ sql/analytics_friendships_daily/analytics_friendships_daily.sql
@@ -10,6 +10,62 @@ SELECT
 					END
 				) AS friendships_created,
 
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 1 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_1d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 7 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_7d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 14 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_14d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 30 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_30d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 60 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_60d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 90 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_90d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= CURRENT_DATE - INTERVAL 180 DAY THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_180d,
+
+				COUNT(DISTINCT
+					CASE
+						WHEN DATE(f.create_date) >= DATE_TRUNC('month', CURRENT_DATE) THEN id
+                        ELSE NULL
+					END
+				) AS friendships_created_cur_month,
+
 				CURRENT_TIMESTAMP AS _loaded_at
 
 FROM            {db_name}.friendship f
